bd2cfc2e0e2dfac6bf280715f6a501d6df071b5b
Sai Kiran <89143127+sai-1712@users.noreply.github.com>
2023-08-16 21:35:43 +0530

    final

--- Question_1/app.js
+++ Question_1/app.js
@@ -33,47 +33,6 @@ axios.post('http://20.244.56.144/train/register', regData)
     console.error('Error:', error);
   });
 
-app.get('/trains', (req, res) => {
-  if (!token) {
-    return res.status(500).json({ error: 'Token not obtained' });
-  }
-
-  const now = new Date();
-  const currentTime = now.getTime();
-  const twelveHoursLater = currentTime + 12 * 60 * 60 * 1000;
-
-  axios.get('http://20.244.56.144/train/trains', {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  })
-  .then(response => {
-    const trains = response.data.filter(train => {
-      const departTime = new Date();
-      departTime.setHours(train.departureTime.Hours);
-      departTime.setMinutes(train.departureTime.Minutes);
-      departTime.setSeconds(train.departureTime.Seconds);
-      const departTimestamp = departTime.getTime() + train.delayedBy * 60 * 1000;
-      return departTimestamp >= currentTime && departTimestamp <= twelveHoursLater;
-    });
-
-    trains.sort((a, b) => {
-      if (a.price.AC === b.price.AC) {
-        if (a.price.sleeper === b.price.sleeper) {
-          return b.seatsAvailable.sleeper - a.seatsAvailable.sleeper;
-        }
-        return a.price.sleeper - b.price.sleeper;
-      }
-      return a.price.AC - b.price.AC;
-    });
-
-    res.status(200).json(trains);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Error fetching data' });
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
